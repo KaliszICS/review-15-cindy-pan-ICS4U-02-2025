03cbb0dfc0514c0956857628bb0693a71710fdb4
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-03-03 05:59:53 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,28 +1,21 @@
+import java.util.HashMap;
+
 public class PracticeProblem {
 
-	public static int goodString(String word){
+	public static int goodString(String s){
 		int amount = -1;
-		boolean check = false;
-		boolean checkAgain = false;
-		int letter = word.length() - 1;
-		if (word.length()>=2){
-			//case one
-			if (word.charAt(0)==word.charAt(letter)){
-				amount = 0;
-			}
-			// case two (first letter)
-			for (int at = 1; at<=letter; at++){
-				if (word.charAt(0)==word.charAt(at)){
-					amount = word.length() - (at+1);
-				}
-			}
-			// case two (second letter)
-			for (int current = 2; current<=letter; current++){
-				if (word.charAt(1)==word.charAt(current)){
-					amount  = (word.length() - (current+1))+1;
-				}
+		int length = s.length();
+		HashMap <Character,Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < length; i ++){
+			map.put((s.charAt(i)),i);
+		}
+		for (int index = 0; index < length; index++){
+			if (map.get(s.charAt(index)) != index){
+				amount = (s.length()-1) - map.get(s.charAt(index)) + index;
+				length = 0;
 			}
 		}
+
 		return amount;
 
 	}
