0617c8b9566225bef25d5ac36281b54d32867f66
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-03-03 06:25:42 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,15 +5,30 @@ public class PracticeProblem {
 	public static int goodString(String s){
 		int amount = -1;
 		int length = s.length();
+		int checker = 0;
+		int amountOne =100, amountTwo =100; 
 		HashMap <Character,Integer> map = new HashMap<Character, Integer>();
 		for (int i = 0; i < length; i ++){
 			map.put((s.charAt(i)),i);
 		}
 		for (int index = 0; index < length; index++){
-			if (map.get(s.charAt(index)) != index){
-				amount = (s.length()-1) - map.get(s.charAt(index)) + index;
+			if (map.get(s.charAt(index)) != index && checker == 0){
+				amountOne = (s.length()-1) - map.get(s.charAt(index)) + index;
+				checker = index;
+			}
+			if (map.get(s.charAt(index)) != index && index > checker){
+				amountTwo = (s.length()-1) - map.get(s.charAt(index)) + index;
 				length = 0;
 			}
+		}
+		if (amountOne<amountTwo){
+			amount = amountOne;
+		}
+		else if (amountOne+amountTwo==200){
+			amount = -1;
+		}
+		else{
+			amount = amountTwo;
 		}
 
 		return amount;
